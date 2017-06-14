16ac1c5338a5552bf0874dc6784ec6640f73567c
cellzero <cellzero@sina.cn>
2017-06-14 23:10:37 +0800

    添加测试场景，以及相应资源。

--- Assets/Scripts/Crow2DFlyController.cs
+++ Assets/Scripts/Crow2DFlyController.cs
@@ -2,92 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crow2DFlyController : MonoBehaviour {
+public class Crow2DFlyController : MonoBehaviour
+{
 
-	public float TimeToRecover;
-	public Camera MainCamera;
-	private float lastPushTime;
-	public float OneStepRight;
+    public float TimeToRecover;
+    private float lastPushTime;
+    public float OneStepRight;
 
-	private Animator _animator;
+    private Animator _animator;
 
-	private float offset_y;
+    // Use this for initialization
+    void Start()
+    {
+        lastPushTime = 0;
 
-	// Use this for initialization
-	void Start () {
+        _animator = this.GetComponent<Animator>();
+    }
 
-		lastPushTime = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation=Quaternion.Euler(transform.rotation.x + 0.5f,transform.rotation.y, transform.rotation.z);    
 
-		_animator = this.GetComponent<Animator>();
+        float cAX = transform.localEulerAngles.x;
 
-		offset_y = MainCamera.transform.position.y - transform.position.y;
-	}
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            lastPushTime = Time.time;
+            if (cAX != 0f && (cAX <= 325f && cAX >= 315f))
+            {
+            }
+            else
 
-	// Update is called once per frame
-	void Update () {
+                transform.Rotate(-1.5f, 0f, 0f);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            lastPushTime = Time.time;
+            if (cAX != 0f && (cAX >= 35F && cAX <= 45f))
+            {
+            }
+            else
+                transform.Rotate(1.5f, 0f, 0f);
+        }
+        else
+        {
+            float curTime = Time.time;
+            if (curTime - lastPushTime > TimeToRecover)
+            {
+                if (cAX >= 300f)
+                {
+                    transform.Rotate(Mathf.Min(0.9f, 360f - cAX), 0f, 0f);
+                }
+                else if (cAX > 0f)
+                {
+                    transform.Rotate(Mathf.Max(-0.9f, -cAX), 0f, 0f);
+                }
+            }
+        }
 
+        cAX = transform.localEulerAngles.x;
 
+        if (cAX >= 270) _animator.SetBool("isUpFlying", true);
+        else _animator.SetBool("isUpFlying", false);
 
-		//transform.rotation=Quaternion.Euler(transform.rotation.x + 0.5f,transform.rotation.y, transform.rotation.z);    
+        float rightSpeed = Mathf.Cos(Mathf.Deg2Rad * cAX);
+        float downSpeed = Mathf.Sin(Mathf.Deg2Rad * cAX);
 
+        Vector3 v = transform.position;
+        v.z = v.z + OneStepRight;
 
+        v.y = v.y - downSpeed / rightSpeed * OneStepRight;
+        if (v.y <= 5f)
+            v.y = 5f;
+        if (v.y >= 75f)
+            v.y = 75f;
 
-		float cAX = transform.localEulerAngles.x;
-
-		if (Input.GetAxis ("Vertical") > 0) {
-			lastPushTime = Time.time;
-			if (cAX != 0f && (cAX <= 325f && cAX >= 315f)) {
-			} else
-
-				transform.Rotate (-2.5f, 0f, 0f);
-		} else if (Input.GetAxis ("Vertical") < 0) {
-			lastPushTime = Time.time;
-			if (cAX != 0f && (cAX >= 35F && cAX <= 45f)) {
-			} else
-				transform.Rotate (2.5f, 0f, 0f);
-		} else {
-			float curTime = Time.time;
-			if (curTime - lastPushTime > TimeToRecover) {
-				if (cAX >= 300f) {
-					transform.Rotate (Mathf.Min(0.9f, 360f - cAX), 0f, 0f);
-				} else if (cAX > 0f) {
-					transform.Rotate (Mathf.Max(-0.9f, -cAX), 0f, 0f);
-				}
-			}
-		}
-
-
-
-
-		cAX = transform.localEulerAngles.x;
-
-
-		if (cAX >=270) _animator.SetBool("isUpFlying", true);
-		else _animator.SetBool("isUpFlying", false);
-
-
-		float rightSpeed = Mathf.Cos (Mathf.Deg2Rad * cAX);
-		float downSpeed = Mathf.Sin (Mathf.Deg2Rad * cAX);
-
-
-
-		Vector3 v = transform.position;
-		v.z = v.z + OneStepRight;
-
-		v.y = v.y - downSpeed / rightSpeed * OneStepRight;
-		if (v.y <= 5f)
-			v.y = 5f;
-		if (v.y >= 75f)
-			v.y = 75f;
-
-		transform.position = v;
-
-		// MainCamera.transform.position = MainCamera.transform.position + new Vector3 (0, 0, OneStepRight);
-
-
-		// MainCamera.transform.position = MainCamera.transform.position + new Vector3 (0, offset_y + transform.position.y - MainCamera.transform.position.y, 0);
-
-
-
-	}
+        transform.position = v;
+    }
 }
---
+++ Assets/_Demos/Scenes/BGTemp/Mirror.shader
@@ -0,0 +1,42 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "GameCore/SpecialEffect/Reflection Mirror"  
+{  
+    Properties {  
+        _ReflectionTex ("Reflection", 2D) = "white" {TexGen ObjectLinear }  
+        _ReflectionColor("Color",Color) = (1,1,1,1)  
+    }  
+    //PC  
+    SubShader {  
+        Tags {  
+            "RenderType"="Opaque"}  
+        LOD 100  
+        Pass {  
+            CGPROGRAM  
+            #pragma vertex vert  
+            #pragma fragment frag  
+            #include "UnityCG.cginc"  
+  
+            uniform float4x4 _ProjMatrix;  
+            uniform sampler2D _ReflectionTex;  
+            float4 _ReflectionColor;  
+            struct outvertex {  
+                float4 pos : SV_POSITION;  
+                float4 uv : TEXCOORD0;  
+            };  
+            outvertex vert(appdata_tan v) {  
+                outvertex o;  
+                o.pos = UnityObjectToClipPos (v.vertex);                  
+                float3 viewDir = ObjSpaceViewDir(v.vertex);  
+                o.uv = mul(_ProjMatrix,float4(viewDir,0));  
+                return o;  
+            }  
+                                      
+            float4 frag(outvertex i) : COLOR {  
+                half4 reflcol = tex2Dproj(_ReflectionTex,i.uv);               
+                return reflcol*_ReflectionColor;  
+            }  
+            ENDCG  
+        }  
+    }  
+}  
