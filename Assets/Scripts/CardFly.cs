dbe811bb846ef4add16b55472af6f5bbc56eb407
6carol6 <mszxw999@gmail.com>
2017-06-16 09:43:09 +0800

    应该飞不过了吧

--- Assets/Scripts/CardFly.cs
+++ Assets/Scripts/CardFly.cs
@@ -43,6 +43,8 @@ public class CardFly : MonoBehaviour {
             speed = 0;
         }
         else if (dist < move_threshold && speed < init_speed) {
+            if (speed + 2 <= init_speed)
+                speed++;
             speed++;
         }
 
